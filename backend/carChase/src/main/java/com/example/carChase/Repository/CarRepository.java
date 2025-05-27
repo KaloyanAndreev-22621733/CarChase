c577ba8981acf0a37d0af08481dc9e75c0245115
Danil Baranov <54190693+GoogleExpert228@users.noreply.github.com>
2025-05-27 16:06:29 +0300

    Delete backend directory

--- backend/carChase/src/main/java/com/example/carChase/Repository/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.carChase.Repository;
-
-import com.example.carChase.model.entity.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-    public Car findCarById(Long id);
-}
